018614e1ae083a6130335d06bfad67bb157ed102
Marc Jermaine Pontiveros <mpontiveros@upd.edu.ph>
2022-05-01 17:25:11 +0800

    trials with pop-ups complete!

--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,69 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
+let currentZone: string;
 let currentPopup: any = undefined;
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
-    
-    WA.room.onEnterLayer('clockZone').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup","It's " + time,[]);
+
+    const config = [
+        {
+            zone: 'unoHelp',
+            message: 'You may play Uno with your friends! Go to the nearby tables and press SPACE when prompted to try!',
+            cta: [
+                {
+                    label: 'That\'s nice!',
+                    className: 'primary',
+                    callback: () => {
+                        if (currentPopup !== undefined) {
+                            currentPopup.close();
+                            currentPopup = undefined;
+                        }
+                    }
+                }
+            ]        
+        }
+    ]
+
+    WA.ui.registerMenuCommand("Tutorial", () => {
+        WA.nav.openTab('https://www.canva.com/design/DAEl4ixLWKA/watch?embed')
     })
 
-    WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
-
+    WA.room.onEnterZone('unoHelp', () => {
+        currentZone = 'unoHelp'
+        openPopup(currentZone, currentZone + 'Popup')
+    });
+    WA.room.onLeaveZone('unoHelp', closePopUp)
+    
+    
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
     }).catch(e => console.error(e));
+
+    function openPopup(zoneName: string, popupName: string) {
+        const zone = config.find((item) => {
+            return item.zone == zoneName
+        });
+        if (typeof zone !== 'undefined') {
+            // @ts-ignore otherwise we can't use zone.cta object
+            currentPopup = WA.ui.openPopup(popupName, zone.message, zone.cta)
+        }
+    }
+
+    function closePopUp(){
+        if (currentPopup !== undefined) {
+            currentPopup.close();
+            currentPopup = undefined;
+        }
+    }
+    
     
 }).catch(e => console.error(e));
 
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
+
+
+
